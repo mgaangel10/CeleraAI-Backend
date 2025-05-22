95267c25d8b1c69cffb1b836c9533ce9ceb59ccb
mgaangel10 <mg.aangel10@gmail.com>
2025-05-22 16:35:06 +0200

    intentando solucionar el error

--- src/main/java/com/example/CeleraAi/OpenAi/service/OpenAIService.java
+++ src/main/java/com/example/CeleraAi/OpenAi/service/OpenAIService.java
@@ -40,9 +40,10 @@ import java.util.stream.Collectors;
 public class OpenAIService {
 
 
-    @Value("${openai.api.key}")
-    private static String API_KEY;
+  @Value("${openai.api.key}")
+  private  String API_KEY;
 
+   // private static final String API_KEY = "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";
 
 
 
@@ -51,8 +52,8 @@ public class OpenAIService {
     
 
     @Value("${openai.api.url}")
-    private  static String API_URL;
-
+    private   String API_URL;
+   // private static final String API_URL = "https://api.openai.com/v1/chat/completions";
 
 
 
